c6b137703f5ca747c06563a6a440455181bd4cd3
takenaga318 <takenaga@chapman.edu>
2020-11-20 13:46:24 -0800

    Adds numbers.sh

--- numbers.sh
+++ numbers.sh
@@ -3,6 +3,11 @@
 # Audrey Takenaga
 echo "Enter a positive interger"
 read -r NUMBER
+while echo "$NUMBER" | grep -E -v "^[1-9][0-9]*$" > /dev/null 2>&1
+do
+	echo "You must enter a positive integer!"
+	read -r NUMBER
+done
 N=1
 while [ "$N" -le "$NUMBER" ]
 do 
